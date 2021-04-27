d4c722d990df907a894d5cecda3eda5af5867d36
3a4oT <petro.rovenskyy@gmail.com>
2021-04-27 22:00:05 +0300

    build project structure. Added test infra

---
+++ tests/health_check_test.rs
@@ -0,0 +1,42 @@
+use std::net::TcpListener;
+
+// `actix_rt::test` is the testing equivalent of `actix_web::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// Use `cargo add actix-rt --dev --vers 2` to add `actix-rt`
+// under `[dev-dependencies]` in Cargo.toml
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check_test` (<- name of the test file)
+#[actix_rt::test]
+async fn health_check_works() {
+    let address: String = spawn_app();
+    let client = reqwest::Client::new();
+    // Act
+    let response = client
+        .get(&format!("{}", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch our application in the background ~somehow~
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port: u16 = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener)
+        .expect("Failed to bind address");
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future,
+    // but we have no use for it here, hence the non-binding let
+    //
+    // New dev dependency - let's add tokio to the party with
+    // `cargo add tokio --dev --vers 1`
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
